0a60f419e3fc3399979c0c4fe8381886d541a93c
yo230899 <agustinustheoo@gmail.com>
2018-03-20 10:27:24 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const hostname = '127.0.0.1';
+const port = 4500;
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+  io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+  });
+  
+  http.listen(port, function(){
+    console.log('Server on port 4500');
+  });
