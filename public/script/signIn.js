7713251792569b73c636ec341c8428168821c0f1
ricky putra harlim <rickyph96>
2018-05-31 15:54:19 +0700

    .

--- public/script/signIn.js
+++ public/script/signIn.js
@@ -1,6 +1,6 @@
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-$('#signin-btn').click(function(e) {
+$('#btnSignIn').click(function(e) {
   e.preventDefault();
   var data = {};
   data.username = $('#username').val();
